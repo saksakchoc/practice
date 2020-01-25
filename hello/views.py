e3b06951c410558e05db92f645e25007c182b7f2
saksakchoc <saksak.ch@gmail.com>
2020-01-25 21:37:29 +0900

    new

---
+++ hello/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'お名前は？',
+    }
+    return render(request, 'hello/index.html', params)
+
+def form(request):
+    msg = request.POST['msg']
+    params = {
+        'title':'Hello/Form',
+        'msg':'こんにちは、'+ msg + 'さん。',
+    }
+    return render(request, 'hello/index.html', params)
